40d6c407d9ecffea021c603756984d58b4b9f37c
PhilippGackstatter <philipp.gackstatter@student.tuwien.ac.at>
2020-08-05 16:27:10 +0800

    Get data from firebase (partial)

--- screens/Profile.js
+++ screens/Profile.js
@@ -1,11 +1,36 @@
-import React from 'react';
-import { StyleSheet, Text, View } from "react-native";
+import React, {Component} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import * as firebase from "firebase";
 
-export default (props) => {
-  return (<View style = {
-    {
-      flex: 1, justifyContent: 'center', alignItems: 'center'
+export default class Profile extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      followers: [],
+      following: [],
+      username: null,
     }
-  }><Text>Profile!</Text>
+  }
+
+  componentDidMount() {
+    const ref = firebase.firestore().collection("users").doc("admin").get();
+
+    ref.then((document) => {
+      console.log(document);
+
+      this.state.followers = document.get("followers");
+      this.state.following = document.get("following");
+      this.state.username = document.get("username");
+
+    });
+  }
+
+  render() {
+    return (<View style = {
+      {
+        flex: 1, justifyContent: 'center', alignItems: 'center'
+      }
+    }><Text>{this.state.username}</Text>
       </View>);
+  }
 }
