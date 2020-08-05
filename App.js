e65bdd7738dffb4db908edd96b6c0052c79bd031
jasperhuangg <jasperhu@usc.edu>
2020-08-05 14:29:38 +0800

    setup firebase config

--- App.js
+++ App.js
@@ -1,8 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import * as firebase from "firebase";
+
+import config from "./constants/config";
 
 export default function App() {
+  useEffect(() => {
+    testFirebase();
+  }, []);
+
+  function testFirebase() {
+    firebase.initializeApp(config.firebaseConfig);
+    const ref = firebase.firestore().collection("users");
+    ref.get().then((snapshot) => {
+      snapshot.docs.forEach((doc) => {
+        console.log(doc.data());
+      });
+    });
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -14,8 +31,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ constants/config.js
@@ -0,0 +1,12 @@
+export default {
+  firebaseConfig: {
+    apiKey: "AIzaSyD0ertZ6WO-wKH1r7i7IM5ncF_5D_kYY0g",
+    authDomain: "recipe-social-network.firebaseapp.com",
+    databaseURL: "https://recipe-social-network.firebaseio.com",
+    projectId: "recipe-social-network",
+    storageBucket: "recipe-social-network.appspot.com",
+    messagingSenderId: "649238189664",
+    appId: "1:649238189664:web:e4e0744d89a6df823fd505",
+    measurementId: "G-DJBE9XZRC3",
+  },
+};
