7c21138d930cb517a1a74c92639ecaa869862426
PhilippGackstatter <philipp.gackstatter@student.tuwien.ac.at>
2020-08-05 15:09:05 +0800

    Add main screen

---
+++ screens/Main.js
@@ -0,0 +1,32 @@
+import React, { useEffect } from "react";
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeScreen from "./screens/Home";
+
+
+function SettingsScreen() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Settings!</Text>
+      </View>
+    );
+  }
+  
+  
+
+const Tab = createBottomTabNavigator();
+
+export default function Main() {
+  return (
+      <NavigationContainer><Tab.Navigator>
+      <Tab.Screen name = 'Home' component =
+       {
+         HomeScreen
+       } />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>);
+}
