b4798f95e41840ade78ffa6fc1f6517b155d0faa
PhilippGackstatter <philipp.gackstatter@student.tuwien.ac.at>
2020-08-06 09:53:16 +0800

    Flesh out Post component

--- screens/Post/Post.js
+++ screens/Post/Post.js
@@ -1,23 +1,46 @@
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { NavigationContainer } from '@react-navigation/native';
-import React from 'react';
-import { SafeAreaView, StyleSheet, Text, View, Image } from 'react-native';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { SafeAreaView, StyleSheet, Text, View, Image } from "react-native";
+import { connect } from "react-redux";
 
-export default (props) => {
+export default class Post extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+      alert(this.props.recipeID);
     return (
-        <View>
-            <Image
-                source={{
-                    uri: 'https://thispersondoesnotexist.com/image',
-                }}
-                />
-             <Text>Lasagne</Text>
-        </View >
-    );
-}
+      <SafeAreaView>
+        <Image
+          source={{
+            uri:
+              "http://www.bpimaging.com/assets/uploads/2016/12/toronto-food-pita.jpg",
+          }}
+        />
+        <Text>Lasagne</Text>
+        <Text>{this.props.recipeID}</Text>
 
+        {/* <Button
+                onPress={() => {
+                  this.props.setViewingRecipe(post.recipeID);
+                  this.props.setViewingRecipeStep(1);
+                  this.props.navigation.navigate("Recipe");
+                  this.props.setTabsShowing(false);
+                }}
+                key={post.recipeID}
+                title={"View Recipe " + post.recipeID}
+                color="#841584"
+              ></Button> */}
+      </SafeAreaView>
+    );
+  }
+}
 
 const styles = {
-    postContainerStyle: { flex: 4, flexDirection: 'row', justifyContent: 'center' }
-}
+  postContainerStyle: {
+    flex: 4,
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+};
