c3488ceff125197f04e78518487beef90d8f9dd0
jasperhuangg <jasperhu@usc.edu>
2020-08-06 10:22:49 +0800

    added query in feed on mount

--- screens/Home.js
+++ screens/Home.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 import { StyleSheet, Text, SafeAreaView } from "react-native";
+import * as firebase from "firebase";
 
 export default (props) => {
-  return (<SafeAreaView style = {
-    {
-      flex: 1, justifyContent: 'center', alignItems: 'center'
-    }
-  }><Text>Home!</Text>
-      </SafeAreaView>);
-}
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    // get all posts from firestore by timestamp
+    const ref = firebase
+      .firestore()
+      .collection("posts")
+      .orderBy("timestamp", "desc")
+      .limitToLast(20)
+      .onSnapshot((snapshot) => {
+        let postArr = [];
+        snapshot.forEach((document) => {
+          console.log(document.data());
+          postArr.push(document.data());
+        });
+        this.setPosts(postArr);
+      });
+  }, []);
+
+  return (
+    <SafeAreaView
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Home!</Text>
+    </SafeAreaView>
+  );
+};
